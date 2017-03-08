7707d14231459b8a49b28a6f39fd57cd95d7b09c
theghall <guategary@gmail.com>
2017-03-08 08:49:26 -0500

    Refactor merge

--- merge-sort.rb
+++ merge-sort.rb
@@ -4,49 +4,42 @@
 #
 require 'byebug'
 
-def merge_sort(ary)
+def merge_sort(ary, merge_ary = [])
   if ary.length > 1
+    # -1 to prevent stack overflow since lengths 2 & 3 will give same mid
+    # point over and over
     mid = ary.length / 2 - 1
 
-    merge_sort(ary[0..mid])
+    merge_sort(ary[0..mid], merge_ary)
 
-    merge_sort(ary[mid + 1..-1])
+    merge_sort(ary[mid + 1..-1], merge_ary)
 
-    p merge(ary[0..mid], ary[mid + 1..-1])
+    puts("merging: #{ary[0..mid]} and #{ary[mid + 1..-1]}")
+    merge_ary = merge(ary[0..mid], ary[mid + 1..-1])
   end
-  ary
+  merge_ary
 end
 
 def merge(b, c)
   merge_ary = []
 
-  puts("b: #{b}")
-  puts("c: #{c}")
   while !b.empty? && !c.empty?
-      b_val = b.first
-
-      c_val = c.first
-
-      if b_val < c_val
+      if b.first < c.first
         merge_ary << b.shift
       else
         merge_ary << c.shift
       end
   end
 
-  if b.empty?
-    merge_ary += c
-  else
-    merge_ary += b
-  end
+  merge_ary += (b.empty? ? c : b)
 
   merge_ary
 end
 
-ary1 = [5,8,3]
-puts("#{ary1}")
+ary1 = [8,3]
+puts("#{ary1}=>")
 p merge_sort(ary1)
 puts("------------")
-ary2 = [8,5,3,1,9,1]
-puts("#{ary2}")
+ary2 = [8,5,3,1]
+puts("#{ary2}=>")
 p merge_sort(ary2)
