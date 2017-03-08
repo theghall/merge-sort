5e7798039947c96d965bead2bbc60675eb751fa6
theghall <guategary@gmail.com>
2017-03-08 01:02:14 -0500

    Fix merge

--- merge-sort.rb
+++ merge-sort.rb
@@ -5,16 +5,16 @@
 require 'byebug'
 
 def merge_sort(ary)
-
   if ary.length > 1
-    mid = (ary.length / 2) - 1
+    mid = ary.length / 2 - 1
 
     merge_sort(ary[0..mid])
+
     merge_sort(ary[mid + 1..-1])
 
-    merge_ary = merge(ary[0..mid], ary[mid + 1..-1])
+    p merge(ary[0..mid], ary[mid + 1..-1])
   end
-  merge_ary
+  ary
 end
 
 def merge(b, c)
@@ -23,18 +23,30 @@ def merge(b, c)
   puts("b: #{b}")
   puts("c: #{c}")
   while !b.empty? && !c.empty?
-      b_val = b.shift
-      c_val = c.shift
+      b_val = b.first
+
+      c_val = c.first
+
       if b_val < c_val
-        merge_ary << b_val << c_val
+        merge_ary << b.shift
       else
-        merge_ary << c_val << b_val
+        merge_ary << c.shift
       end
   end
 
-  p merge_ary
+  if b.empty?
+    merge_ary += c
+  else
+    merge_ary += b
+  end
+
   merge_ary
 end
 
-p merge_sort([8,5,3])
-#p merge_sort([8,5,3,1,9,7])
+ary1 = [5,8,3]
+puts("#{ary1}")
+p merge_sort(ary1)
+puts("------------")
+ary2 = [8,5,3,1,9,1]
+puts("#{ary2}")
+p merge_sort(ary2)
