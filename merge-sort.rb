16ed2877c43eb9f1179a19b81b87342f2fb6fe8d
theghall <guategary@gmail.com>
2017-03-08 22:21:50 -0500

    Refactor form Rubocop warnings

--- merge-sort.rb
+++ merge-sort.rb
@@ -1,47 +1,41 @@
-#merge-sort.rb
+# merge-sort.rb
 #
-#20170307	 GH
+# 20170307	 GH
 #
-require 'byebug'
 
-def merge_sort(ary, merge_ary = [])
-  if ary.length > 1
-    # -1 to prevent stack overflow since lengths 2 & 3 will give same mid
-    # point over and over
-    mid = ary.length / 2 - 1
+def merge_sort(ary)
+  return ary if ary.length == 1
 
-    left = ary[0..mid]
-    right = ary[mid + 1..-1]
+  # -1 to prevent stack overflow since lengths 2 & 3 will give same mid
+  # point over and over
+  mid = ary.length / 2 - 1
 
-    merge_sort(left, merge_ary)
+  left = ary[0..mid]
 
-    merge_sort(right, merge_ary)
+  right = ary[mid + 1..-1]
 
-    merge_ary = merge(left, right)
-  end
-  merge_ary
+  left = merge_sort(left)
+
+  right = merge_sort(right)
+
+  merge(left, right)
 end
 
 def merge(b, c)
   merge_ary = []
 
   while !b.empty? && !c.empty?
-      if b.first < c.first
-        merge_ary << b.shift
-      else
-        merge_ary << c.shift
-      end
+    merge_ary << (b.first < c.first ? b.shift : c.shift)
   end
 
-  merge_ary += (b.empty? ? c : b)
-
-  merge_ary
+  merge_ary + (b.empty? ? c : b)
 end
 
-ary1 = [8,5]
-puts("#{ary1}=>")
-p merge_sort(ary1)
-puts("----------")
-ary2 = [8,5,3]
-puts("#{ary2}=>")
-p merge_sort(ary2)
+puts("Let's rock the merge sort!")
+[[8, 5], [8, 5, 3], [8, 5, 3, 9, 1, 7]].each do |ary|
+  puts("#{ary}")
+
+  merge_ary = merge_sort(ary)
+
+  puts("=> #{merge_ary}\n\n")
+end
